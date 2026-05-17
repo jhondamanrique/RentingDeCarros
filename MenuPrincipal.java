2dbdbc690569af0f5a6e0afbb3b1445c1626d4d8
jhon manrique <jhondamanrique@gmail.com>
2026-05-17 18:04:34 -0500

    Creacion rama HU1

---
+++ MenuPrincipal.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class MenuPrincipal {
+    public static void Menu(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Sistema Renting de Carros - Menu Principal");
+    }
+}
